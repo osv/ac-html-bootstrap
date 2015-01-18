a5b8a89ab8e86bb68d2e63b198055a3daa83c814
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-01-18 21:30:26 +0200

    Text

--- ac-html-bootstrap.el
+++ ac-html-bootstrap.el
@@ -24,7 +24,7 @@
 ;;; Commentary:
 
 ;;   If  you ok  that  "glyphicon"  classes allowed  only  for <i>  or
-;; "label-default" for  <span>, <label>  and so on  this may  good for
+;; "label-default" for  <span>, <label> and  so on, this may  good for
 ;; you.   Alternative you  can use  package `ac-html-csswatcher'  that
 ;; parse   all  css   in   prject.   However   this  package   provide
 ;; documentation  with samples  and additional  data- attributes  that
